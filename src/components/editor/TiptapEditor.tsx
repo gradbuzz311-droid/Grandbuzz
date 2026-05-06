daccbd42795596b7283b518ae713213982362260
asrith2208 <uudoonasrith@gmail.com>
2026-05-06 23:46:53 +0530

    Fix TextStyle import in TiptapEditor

--- src/components/editor/TiptapEditor.tsx
+++ src/components/editor/TiptapEditor.tsx
@@ -7,7 +7,7 @@ import Image from "@tiptap/extension-image";
 import Link from "@tiptap/extension-link";
 import Placeholder from "@tiptap/extension-placeholder";
 import Color from "@tiptap/extension-color";
-import TextStyle from "@tiptap/extension-text-style";
+import { TextStyle } from "@tiptap/extension-text-style";
 import Highlight from "@tiptap/extension-highlight";
 import { 
   Bold, Italic, Underline as UnderlineIcon, Strikethrough, 
